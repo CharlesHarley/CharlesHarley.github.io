2abf90bff8b4c8b63617944e3256b528be7fe151
Charles Harley <charles.harley@googlemail.com>
2020-09-15 18:28:20 +0100

    Add Tailwind CSS framework
    Uses webpack to process the source CSS and generate the final CSS file
    to be linked to by the Jekyll theme.

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    entry: {
+        main: path.resolve(__dirname, './_styles/main.css'),
+    },
+    output: {
+        path: path.resolve(__dirname, './assets/styles/'),
+        filename: isProduction ? '[name].[hash].js' : '[name].js',
+        chunkFilename: isProduction ? '[id].[hash].js' : '[id].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: isProduction ? '[name].[hash].css' : '[name].css'
+        }),
+        new ManifestPlugin({
+            fileName: '../../_data/manifest.yml',
+            publicPath: './assets/styles/',
+        }),
+        new FileManagerPlugin({
+            onEnd: {
+                delete: [
+                    // Deleting the JS file as we're only interested in the CSS file.
+                    './assets/styles/main.js',
+                ],
+            }
+        }),
+    ],
+};
