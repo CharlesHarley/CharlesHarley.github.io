ed21158bde2e197f43a95a63559ab58b60a47dbd
Charles Harley <charles.harley@googlemail.com>
2020-09-20 15:41:10 +0100

    Set max-width to the medium screen size
    Also tweaked the colours of the footer background and borders

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '800px',
+            'lg': '1024px',
+            'xl': '1280px',
+        }
+    }
+}
